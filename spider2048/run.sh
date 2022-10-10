2c0f9931aee707d058d777832ab832d9ecb498cd
gwq5210 <gwq5210@qq.com>
2022-10-10 21:59:49 +0800

    update run.sh

--- spider2048/run.sh
+++ spider2048/run.sh
@@ -2,5 +2,5 @@ echo "" > images.log
 mirai_api_key=
 es_password=
 scrapyd_password=
-scrapy crawl images -s MIRAI_API_KEY=$mirai_api_key -s ES_PASSWORD=$es_password --logfile=images.log
-# curl https://gwq5210:$scrapyd_password@gwq5210.com:6800/schedule.json -d project=spider2048 -d spider=images -d setting=ES_PASSWORD=$es_password -d setting=MIRAI_API_KEY=$mirai_api_key -d setting=LOG_LEVEL=INFO -d setting=PAGE_LIMIT_COUNT=1
+# scrapy crawl images -s MIRAI_API_KEY=$mirai_api_key -s ES_PASSWORD=$es_password --logfile=images.log
+curl https://scrapyd.gwq5210.com/schedule.json -u gwq5210:$scrapyd_password -d project=spider2048 -d spider=images -d setting=ES_PASSWORD=$es_password -d setting=MIRAI_API_KEY=$mirai_api_key -d setting=LOG_LEVEL=INFO -d setting=PAGE_LIMIT_COUNT=100
--- spider_wexyx/run.sh
+++ spider_wexyx/run.sh
@@ -2,5 +2,5 @@ echo "" > nes.log
 mirai_api_key=
 es_password=
 scrapyd_password=
-scrapy crawl nes -s MIRAI_API_KEY=$mirai_api_key -s ES_PASSWORD=$es_password --logfile=nes.log
-# curl https://gwq5210:$scrapyd_password@gwq5210.com:6800/schedule.json -d project=spider_wexyx -d spider=nes -d setting=ES_PASSWORD=$es_password -d setting=MIRAI_API_KEY=$mirai_api_key -d setting=LOG_LEVEL=DEBUG
+# scrapy crawl nes -s MIRAI_API_KEY=$mirai_api_key -s ES_PASSWORD=$es_password --logfile=nes.log
+curl https://scrapyd.gwq5210.com/schedule.json -u gwq5210:$scrapyd_password -d project=spider_wexyx -d spider=nes -d setting=ES_PASSWORD=$es_password -d setting=MIRAI_API_KEY=$mirai_api_key -d setting=LOG_LEVEL=DEBUG
